33668dd1fe02e690465f54b578be0f6ba32e699e
Manish <manish.dalal@moonshinetechnology.com>
2024-09-28 21:27:36 +0530

    Interval changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const methods = require('./clicker');
+const axios = require('axios');
 
 const { clicker } = methods;
 
@@ -9,6 +10,10 @@ app.set('port', process.env.PORT || 4000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
 app.get('/airdrop/clicker', (req, res) => {
   clicker();
   res.send('clicker started');
@@ -16,6 +21,38 @@ app.get('/airdrop/clicker', (req, res) => {
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+const startHamsterWakeScript = async () => {
+  await axios.get('https://hamster-1.glitch.me/wake-other');
+  await sleep(60000);
+  await axios.get('https://hamster-1.glitch.me/wake-other');
+  await sleep(10000);
+  await axios.get('https://hamster-1.glitch.me/claim-other');
+  await sleep(60000);
+  await axios.get('https://hamster-1.glitch.me/wake-other');
+};
+
+const randomIntFromInterval = (min, max) => {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+let oldInterval = null;
+let randomTimeout = randomIntFromInterval(3, 28);
+
+
+app.get('/start-hamster-bots', (req, res) => {
+  const maxStartTime = parseInt(req.query && req.query.maxTime) || 28;
+  oldInterval && clearTimeout(oldInterval);
+  randomTimeout = randomIntFromInterval(3, maxStartTime);
+  console.log('Starts in ', randomTimeout, ' mins');
+
+  oldInterval = setTimeout(startHamsterWakeScript, 60000 * randomTimeout);
+  res.send('Started hamster bots');
+});
+
+app.get('/get-next-start', (req, res) => {
+  res.send(`${randomTimeout}`);
 });
 
 app.listen(app.get('port'), () => {
